5af37274ea737d92242ee2c18cd88ac682659fee
rycox <ryan.cox.312@gmail.com>
2025-04-09 10:17:59 -0400

    testmessage

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1744205927',resources: [
+    cacheName: 'chirpy-1744206288',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
